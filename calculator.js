8b4aa1d5736c9523ea90bbc5a9a655ec3fd91fe3
Samuel McKean <samuel.mckean@gmail.com>
2020-10-17 08:37:30 -0500

    Finished adding buttons

---
+++ calculator.js
@@ -0,0 +1,24 @@
+// adds two values
+function add(a, b) {
+  return a + b;
+}
+
+// subtracts two values
+function subtract(a, b) {
+  return a - b;
+}
+
+// multiplies two values
+function multiply(a, b) {
+  return a * b;
+}
+
+// divides two values
+function divide(a, b) {
+  return a / b;
+}
+
+// takes an math operator and two values and applies the operator to the numbers
+function operate(operator, a, b) {
+  return operator(a, b);
+}
